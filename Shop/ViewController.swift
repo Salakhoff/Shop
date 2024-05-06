99e07962ef4730f2f9b4bd07b744983377e98d54
Ilfat <salakhoff.ilfat@gmail.com>
2024-05-06 05:10:25 +0300

    First Settings

--- Shop/ViewController.swift
+++ Shop/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Shop
-//
-//  Created by Ильфат Салахов on 06.05.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
